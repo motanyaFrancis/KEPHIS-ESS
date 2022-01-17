e99ad6dec5a322ed2bb025211ea401286f6e45b8
enockabere <maebaenock95@gmail.com>
2022-01-17 10:41:40 +0300

    create dashboard charts

--- accounts/views.py
+++ accounts/views.py
@@ -10,36 +10,12 @@ import datetime
 
 
 def profile_request(request):
-    session = requests.Session()
-    session.auth = config.AUTHS
 
-    Access_Point = config.O_DATA.format("/UpcomingEvents")
-    response = session.get(Access_Point).json()
-
-    res = response['value']
-    # Get Timezone
-    # creating date object
     todays_date = datetime.datetime.now().strftime("%b. %d, %Y %A")
-    ctx = {"today": todays_date, "res": res}
+    ctx = {"today": todays_date}
     return render(request, 'profile.html', ctx)
 
 
 def login_request(request):
-    '''
-    In order to catch exception well, make sure to know what every attribute contains
-    '''
-    customerNo = '01-00334545'
-    eventNo = 'ev00030344'
-    RegNo = 'null'
-    try:
-        if customerNo != '' and eventNo != '':
-            result = config.CLIENT.service.RegisterEvent(
-                customerNo, eventNo, RegNo)
-            print(result)
-            notify = "Successfully Added"
-        else:
-            raise ValueError('Incorrect input!')
-    except Exception as e:
-        notify = e
-    ctx = {"note": notify}
-    return render(request, 'auth.html', ctx)
+
+    return render(request, 'auth.html')
