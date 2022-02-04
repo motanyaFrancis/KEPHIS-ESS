a980e50b54bc2dad934149adb15139ffc396431e
enockabere <maebaenock95@gmail.com>
2022-02-04 14:23:26 +0300

    consume surrender imprest

--- Finance/views.py
+++ Finance/views.py
@@ -20,6 +20,7 @@ def ImprestRequisition(request):
         response = session.get(Access_Point, timeout=10).json()
         open = []
         Approved = []
+        Rejected = []
         for tender in response['value']:
             if tender['Status'] == 'Open':
                 output_json = json.dumps(tender)
@@ -27,6 +28,9 @@ def ImprestRequisition(request):
             if tender['Status'] == 'Released':
                 output_json = json.dumps(tender)
                 Approved.append(json.loads(output_json))
+            if tender['Status'] == 'Rejected':
+                output_json = json.dumps(tender)
+                Rejected.append(json.loads(output_json))
         counts = len(open)
         counter = len(Approved)
     except requests.exceptions.ConnectionError as e:
@@ -40,10 +44,37 @@ def ImprestRequisition(request):
 
 def ImprestSurrender(request):
 
-    # Get Timezone
-    # creating date object
+    session = requests.Session()
+    session.auth = config.AUTHS
+
+    Access_Point = config.O_DATA.format("/QyImprestSurrenders")
+    Access_Point2 = config.O_DATA.format("/Imprests")
+    try:
+        response = session.get(Access_Point, timeout=10).json()
+        r = session.get(Access_Point2, timeout=10).json()
+        open = []
+        Approved = []
+        New = []
+        for tender in response['value']:
+            if tender['Status'] == 'Open':
+                output_json = json.dumps(tender)
+                open.append(json.loads(output_json))
+            if tender['Status'] == 'Released':
+                output_json = json.dumps(tender)
+                Approved.append(json.loads(output_json))
+        for tender in r['value']:
+            if tender['Status'] == 'Released':
+                output_json = json.dumps(tender)
+                New.append(json.loads(output_json))
+        counts = len(open)
+        counter = len(Approved)
+        counts = len(New)
+    except requests.exceptions.ConnectionError as e:
+        print(e)
+
     todays_date = datetime.datetime.now().strftime("%b. %d, %Y %A")
-    ctx = {"today": todays_date}
+    ctx = {"today": todays_date, "res": open, "count": counts,
+           "response": Approved, "counter": counter, 'new': New, "counts": counts}
     return render(request, 'imprestSurr.html', ctx)
 
 
