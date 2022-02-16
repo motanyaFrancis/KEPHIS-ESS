bd2d85ca581fe595b0c1af8af86c46803e21b435
enockabere <maebaenock95@gmail.com>
2022-02-17 01:26:30 +0300

    finish approvals

--- Approvals/urls.py
+++ Approvals/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('LeaveApproval', views.LeaveApproval, name="leaveApp"),
-    path('TrainingApproval', views.TrainingApproval, name="TrainApp"),
-    path('LoanApproval', views.LoansApproval, name="LoanApp"),
+    path('Approve', views.Approve, name="approve"),
+    path('ApproveData/<str:pk>', views.ApproveDetails, name='ApproveData'),
+    path('Approved/<str:pk>', views.All_Approved, name='All_Approved'),
+    path('Rejected/<str:pk>', views.Rejected, name='Rejected'),
 ]
--- Approvals/views.py
+++ Approvals/views.py
@@ -5,27 +5,98 @@ from requests import Session
 from requests_ntlm import HttpNtlmAuth
 import json
 from django.conf import settings as config
-import datetime
+import datetime as dt
+from django.contrib import messages
 
 # Create your views here.
 
 
-def LeaveApproval(request):
+def Approve(request):
+    session = requests.Session()
+    session.auth = config.AUTHS
 
-    todays_date = datetime.datetime.now().strftime("%b. %d, %Y %A")
-    ctx = {"today": todays_date}
-    return render(request, 'leaveApproval.html', ctx)
+    Access_Point = config.O_DATA.format("/QyApprovalEntries")
+    try:
+        response = session.get(Access_Point, timeout=10).json()
+        open = []
+        for approve in response['value']:
+            if approve['Status'] == 'Open' and approve['Approver_ID'] == request.session['User_ID']:
+                output_json = json.dumps(approve)
+                open.append(json.loads(output_json))
+        counts = len(open)
+    except requests.exceptions.ConnectionError as e:
+        print(e)
+
+    todays_date = dt.datetime.now().strftime("%b. %d, %Y %A")
+    ctx = {"today": todays_date, "res": open, "count": counts}
+    return render(request, 'Approve.html', ctx)
 
 
-def TrainingApproval(request):
+def ApproveDetails(request, pk):
+    session = requests.Session()
+    session.auth = config.AUTHS
+    res = ''
+    Access_Point = config.O_DATA.format("/QyApprovalEntries")
+    try:
+        response = session.get(Access_Point, timeout=10).json()
+        Approves = []
+        for approve in response['value']:
+            if approve['Status'] == 'Open' and approve['Approver_ID'] == request.session['User_ID']:
+                output_json = json.dumps(approve)
+                Approves.append(json.loads(output_json))
+                for claim in Approves:
+                    if claim['Document_No_'] == pk:
+                        res = claim
+    except requests.exceptions.ConnectionError as e:
+        print(e)
+    todays_date = dt.datetime.now().strftime("%b. %d, %Y %A")
+    ctx = {"today": todays_date, "res": res}
+    return render(request, 'approveDetails.html', ctx)
 
-    todays_date = datetime.datetime.now().strftime("%b. %d, %Y %A")
-    ctx = {"today": todays_date}
-    return render(request, 'trainingApproval.html', ctx)
+
+def All_Approved(request, pk):
+    entryNo = 0
+    documentNo = pk
+    userID = request.session['User_ID']
+    approvalComments = ""
+    myAction = 'approve'
+    if request.method == 'POST':
+        try:
+            approvalComments = request.POST.get('approvalComments')
+        except ValueError:
+            messages.error(request, "Not sent. Invalid Input, Try Again!!")
+            return redirect('IMPDetails', pk=documentNo)
+    try:
+        response = config.CLIENT.service.FnDocumentApproval(
+            entryNo, documentNo, userID, approvalComments, myAction)
+        messages.success(request, "Successfully Added!!")
+        print(response)
+        return redirect('ApproveData', pk=documentNo)
+    except Exception as e:
+        messages.error(request, e)
+        print(e)
+    return redirect('ApproveData', pk=documentNo)
 
 
-def LoansApproval(request):
-
-    todays_date = datetime.datetime.now().strftime("%b. %d, %Y %A")
-    ctx = {"today": todays_date}
-    return render(request, 'loansApproval.html', ctx)
+def Rejected(request, pk):
+    entryNo = 0
+    documentNo = pk
+    userID = request.session['User_ID']
+    approvalComments = ""
+    myAction = 'reject'
+    if request.method == 'POST':
+        try:
+            approvalComments = request.POST.get('approvalComments')
+        except ValueError:
+            messages.error(request, "Not sent. Invalid Input, Try Again!!")
+            return redirect('IMPDetails', pk=documentNo)
+    try:
+        response = config.CLIENT.service.FnDocumentApproval(
+            entryNo, documentNo, userID, approvalComments, myAction)
+        messages.success(request, "Successfully Added!!")
+        print(response)
+        return redirect('ApproveData', pk=documentNo)
+    except Exception as e:
+        messages.error(request, e)
+        print(e)
+    return redirect('ApproveData', pk=documentNo)
