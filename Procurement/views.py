1d79a43c969343b913a68dabc6f8e5c65b587cdc
enockabere <maebaenock95@gmail.com>
2022-02-17 12:54:19 +0300

    create purchase requisition get request

--- Procurement/views.py
+++ Procurement/views.py
@@ -5,27 +5,54 @@ from requests import Session
 from requests_ntlm import HttpNtlmAuth
 import json
 from django.conf import settings as config
-import datetime
+import datetime as dt
 
 # Create your views here.
 
 
 def PurchaseRequisition(request):
+    session = requests.Session()
+    session.auth = config.AUTHS
 
-    todays_date = datetime.datetime.now().strftime("%b. %d, %Y %A")
-    ctx = {"today": todays_date}
+    Access_Point = config.O_DATA.format("/QyLeaveApplications")
+    try:
+        response = session.get(Access_Point, timeout=10).json()
+        open = []
+        Approved = []
+        Rejected = []
+        for imprest in response['value']:
+            if imprest['Status'] == 'Open' and imprest['User_ID'] == request.session['User_ID']:
+                output_json = json.dumps(imprest)
+                open.append(json.loads(output_json))
+            if imprest['Status'] == 'Released' and imprest['User_ID'] == request.session['User_ID']:
+                output_json = json.dumps(imprest)
+                Approved.append(json.loads(output_json))
+            if imprest['Status'] == 'Rejected' and imprest['User_ID'] == request.session['User_ID']:
+                output_json = json.dumps(imprest)
+                Rejected.append(json.loads(output_json))
+        counts = len(open)
+        counter = len(Approved)
+        reject = len(Rejected)
+    except requests.exceptions.ConnectionError as e:
+        print(e)
+
+    todays_date = dt.datetime.now().strftime("%b. %d, %Y %A")
+    ctx = {"today": todays_date, "res": open, "count": counts,
+           "response": Approved, "counter": counter, "rej": Rejected,
+           'reject': reject}
+
     return render(request, 'purchaseReq.html', ctx)
 
 
 def RepairRequest(request):
 
-    todays_date = datetime.datetime.now().strftime("%b. %d, %Y %A")
+    todays_date = dt.datetime.now().strftime("%b. %d, %Y %A")
     ctx = {"today": todays_date}
     return render(request, 'repairReq.html', ctx)
 
 
 def StoreRequest(request):
 
-    todays_date = datetime.datetime.now().strftime("%b. %d, %Y %A")
+    todays_date = dt.datetime.now().strftime("%b. %d, %Y %A")
     ctx = {"today": todays_date}
     return render(request, 'storeReq.html', ctx)
