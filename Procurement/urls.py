a90c7b72f90b42c78b3ce8addf6c3da972da425b
enockabere <maebaenock95@gmail.com>
2022-02-17 13:07:52 +0300

    create purchase requisition post request

--- Procurement/urls.py
+++ Procurement/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('PurchaseRequisition', views.PurchaseRequisition, name='purchase'),
+    path('PurchaseDetail/<str:pk>',
+         views.PurchaseRequestDetails, name='PurchaseDetail'),
     path('RepairRequest', views.RepairRequest, name='repair'),
     path('StoreRequest', views.StoreRequest, name='store'),
 ]
--- Procurement/views.py
+++ Procurement/views.py
@@ -44,6 +44,56 @@ def PurchaseRequisition(request):
     return render(request, 'purchaseReq.html', ctx)
 
 
+def PurchaseRequestDetails(request, pk):
+    session = requests.Session()
+    session.auth = config.AUTHS
+    state = ''
+    res = ''
+    Access_Point = config.O_DATA.format("/QyLeaveApplications")
+    try:
+        response = session.get(Access_Point, timeout=10).json()
+
+        openImp = []
+        res_type = []
+        for imprest in response['value']:
+            if imprest['Status'] == 'Released' and imprest['User_ID'] == request.session['User_ID']:
+                output_json = json.dumps(imprest)
+                openImp.append(json.loads(output_json))
+                for imprest in openImp:
+                    if imprest['Application_No'] == pk:
+                        res = imprest
+            if imprest['Status'] == 'Open' and imprest['User_ID'] == request.session['User_ID']:
+                output_json = json.dumps(imprest)
+                openImp.append(json.loads(output_json))
+                for imprest in openImp:
+                    if imprest['Application_No'] == pk:
+                        res = imprest
+                        if imprest['Status'] == 'Open':
+                            state = 1
+            if imprest['Status'] == 'Released' and imprest['User_ID'] == request.session['User_ID']:
+                output_json = json.dumps(imprest)
+                openImp.append(json.loads(output_json))
+                for imprest in openImp:
+                    if imprest['Application_No'] == pk:
+                        res = imprest
+    except requests.exceptions.ConnectionError as e:
+        print(e)
+    Lines_Res = config.O_DATA.format("/QyImprestSurrenderLines")
+    try:
+        response_Lines = session.get(Lines_Res, timeout=10).json()
+        openLines = []
+        for imprest in response_Lines['value']:
+            if imprest['AuxiliaryIndex1'] == pk:
+                output_json = json.dumps(imprest)
+                openLines.append(json.loads(output_json))
+    except requests.exceptions.ConnectionError as e:
+        print(e)
+    todays_date = dt.datetime.now().strftime("%b. %d, %Y %A")
+    ctx = {"today": todays_date, "res": res,
+           "state": state, "line": openLines, "type": res_type}
+    return render(request, 'purchaseDetail.html', ctx)
+
+
 def RepairRequest(request):
 
     todays_date = dt.datetime.now().strftime("%b. %d, %Y %A")
