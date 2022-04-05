ab164cfc625d711c36311e05fe07b2bf1b80940f
enockabere <maebaenock95@gmail.com>
2022-04-05 14:07:03 +0300

    create training request stats

--- dashboard/views.py
+++ dashboard/views.py
@@ -13,19 +13,26 @@ def dashboard(request):
     session = requests.Session()
     session.auth = config.AUTHS
 
-    Access_Point = config.O_DATA.format("/Imprests")
+    Access_Imprest = config.O_DATA.format("/Imprests")
     Access_Leave = config.O_DATA.format("/QyLeaveApplications")
+    Access_Train = config.O_DATA.format("/QyTrainingRequests")
     try:
-        response = session.get(Access_Point, timeout=10).json()
+        Imprest = session.get(Access_Imprest, timeout=10).json()
         Leave = session.get(Access_Leave, timeout=10).json()
+        Training = session.get(Access_Train, timeout=10).json()
+
         openLeave = []
         AppLeave = []
         RejLeave = []
+
         openTraining = []
         AppTraining = []
         RejTraining = []
-        Approved = []
-        Open = []
+
+        openImprest = []
+        AppImprest = []
+        RejImprest = []
+
         for Leave in Leave['value']:
             if Leave['Status'] == 'Open' and Leave['User_ID'] == request.session['User_ID']:
                 output_json = json.dumps(Leave)
@@ -36,28 +43,40 @@ def dashboard(request):
             if Leave['Status'] == 'Rejected' and Leave['User_ID'] == request.session['User_ID']:
                 output_json = json.dumps(Leave)
                 RejLeave.append(json.loads(output_json))
-        for imprest in response['value']:
-            if imprest['Status'] == 'Open' and imprest['Employee_No'] == request.session['Employee_No_']:
+
+        for Training in Training['value']:
+            if Training['Status'] == 'Open' and Training['Employee_No'] == request.session['Employee_No_']:
+                output_json = json.dumps(Training)
+                openTraining.append(json.loads(output_json))
+            if Training['Status'] == 'Released' and Training['Employee_No'] == request.session['Employee_No_']:
+                output_json = json.dumps(Training)
+                AppTraining.append(json.loads(output_json))
+            if Training['Status'] == 'Rejected' and Training['Employee_No'] == request.session['Employee_No_']:
+                output_json = json.dumps(Training)
+                RejTraining.append(json.loads(output_json))
+
+        for imprest in Imprest['value']:
+            if imprest['Status'] == 'Open' and imprest['User_Id'] == request.session['User_ID']:
                 output_json = json.dumps(imprest)
                 open.append(json.loads(output_json))
-            if imprest['Status'] == 'Released' and imprest['Employee_No'] == request.session['Employee_No_']:
+            if imprest['Status'] == 'Released' and imprest['User_Id'] == request.session['User_ID']:
                 output_json = json.dumps(imprest)
                 Approved.append(json.loads(output_json))
-            if imprest['Status'] == 'Rejected' and imprest['Employee_No'] == request.session['Employee_No_']:
+            if imprest['Status'] == 'Rejected' and imprest['User_Id'] == request.session['User_ID']:
                 output_json = json.dumps(imprest)
                 Rejected.append(json.loads(output_json))
-        for tender in response['value']:
-            if tender['Status'] == 'Open':
-                output_json = json.dumps(tender)
-                Open.append(json.loads(output_json))
-            if tender['Status'] == 'Released':
-                output_json = json.dumps(tender)
-                Approved.append(json.loads(output_json))
+
         counts = len(Open)
         counter = len(Approved)
+
         leave_open = len(openLeave)
         leave_App = len(AppLeave)
         leave_rej = len(RejLeave)
+
+        train_open = len(openTraining)
+        train_app = len(AppTraining)
+        train_rej = len(RejTraining)
+
     except requests.exceptions.ConnectionError as e:
         print(e)
 
@@ -87,7 +106,9 @@ def dashboard(request):
            "E_Mail": E_Mail, "Employee_No_": Employee_No_,
            "Customer_No_": Customer_No_, "apps": Approve, "countsAPP": countsAPP,
            "leave_open": leave_open, "leave_app": leave_App,
-           "leave_rej": leave_rej}
+           "leave_rej": leave_rej, "open_train": train_open,
+           "app_train": train_app, "rej_train": train_rej
+           }
     return render(request, 'main/dashboard.html', ctx)
 
 
