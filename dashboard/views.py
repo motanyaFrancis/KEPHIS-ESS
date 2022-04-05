77bd60059e815ac64bd3090d13eff6137eba13b8
enockabere <maebaenock95@gmail.com>
2022-04-05 17:05:44 +0300

    add dashboard styles

--- dashboard/views.py
+++ dashboard/views.py
@@ -17,11 +17,20 @@ def dashboard(request):
     Access_Leave = config.O_DATA.format("/QyLeaveApplications")
     Access_Train = config.O_DATA.format("/QyTrainingRequests")
     Access_Surrender = config.O_DATA.format("/QyImprestSurrenders")
+    Access_Claim = config.O_DATA.format("/QyStaffClaims")
+    Access_purchase = config.O_DATA.format("/QyPurchaseRequisitionHeaders")
+    Access_Repair = config.O_DATA.format("/QyRepairRequisitionHeaders")
+    Access_Store = config.O_DATA.format("/QyStoreRequisitionHeaders")
+
     try:
         Imprest = session.get(Access_Imprest, timeout=10).json()
         Leave = session.get(Access_Leave, timeout=10).json()
         Training = session.get(Access_Train, timeout=10).json()
         Surrender = session.get(Access_Surrender, timeout=10).json()
+        Claim = session.get(Access_Claim, timeout=10).json()
+        Purchase = session.get(Access_purchase, timeout=10).json()
+        Repair = session.get(Access_Repair, timeout=10).json()
+        Store = session.get(Access_Store, timeout=10).json()
 
         openLeave = []
         AppLeave = []
@@ -38,6 +47,22 @@ def dashboard(request):
         openSurrender = []
         AppSurrender = []
         RejSurrender = []
+
+        openClaim = []
+        AppClaim = []
+        RejClaim = []
+
+        openPurchase = []
+        AppPurchase = []
+        RejPurchase = []
+
+        openRepair = []
+        AppRepair = []
+        RejRepair = []
+
+        openStore = []
+        AppStore = []
+        RejStore = []
 
         for Leave in Leave['value']:
             if Leave['Status'] == 'Open' and Leave['User_ID'] == request.session['User_ID']:
@@ -81,6 +106,46 @@ def dashboard(request):
             if Surrender['Status'] == 'Rejected' and Surrender['User_Id'] == request.session['User_ID']:
                 output_json = json.dumps(imprest)
                 RejSurrender.append(json.loads(output_json))
+        for Claim in Claim['value']:
+            if Claim['Status'] == 'Open' and Claim['User_Id'] == request.session['User_ID']:
+                output_json = json.dumps(imprest)
+                openClaim.append(json.loads(output_json))
+            if Claim['Status'] == 'Released' and Claim['User_Id'] == request.session['User_ID']:
+                output_json = json.dumps(imprest)
+                AppClaim.append(json.loads(output_json))
+            if Claim['Status'] == 'Rejected' and Claim['User_Id'] == request.session['User_ID']:
+                output_json = json.dumps(imprest)
+                RejClaim.append(json.loads(output_json))
+        for Purchase in Purchase['value']:
+            if Purchase['Status'] == 'Open' and Purchase['Employee_No_'] == request.session['Employee_No_']:
+                output_json = json.dumps(Purchase)
+                openPurchase.append(json.loads(output_json))
+            if Purchase['Status'] == 'Released' and Purchase['Employee_No_'] == request.session['Employee_No_']:
+                output_json = json.dumps(Purchase)
+                AppPurchase.append(json.loads(output_json))
+            if Purchase['Status'] == 'Rejected' and Purchase['Employee_No_'] == request.session['Employee_No_']:
+                output_json = json.dumps(Purchase)
+                RejPurchase.append(json.loads(output_json))
+        for Repair in Repair['value']:
+            if Repair['Status'] == 'Open' and Repair['Requested_By'] == request.session['User_ID']:
+                output_json = json.dumps(Repair)
+                openRepair.append(json.loads(output_json))
+            if Repair['Status'] == 'Released' and Repair['Requested_By'] == request.session['User_ID']:
+                output_json = json.dumps(Repair)
+                AppRepair.append(json.loads(output_json))
+            if Repair['Status'] == 'Rejected' and Repair['Requested_By'] == request.session['User_ID']:
+                output_json = json.dumps(Repair)
+                RejRepair.append(json.loads(output_json))
+        for Store in Store['value']:
+            if Store['Status'] == 'Open' and Store['Requested_By'] == request.session['User_ID']:
+                output_json = json.dumps(Store)
+                openStore.append(json.loads(output_json))
+            if Store['Status'] == 'Released' and Store['Requested_By'] == request.session['User_ID']:
+                output_json = json.dumps(Store)
+                AppStore.append(json.loads(output_json))
+            if Store['Status'] == 'Rejected' and Store['Requested_By'] == request.session['User_ID']:
+                output_json = json.dumps(Store)
+                RejStore.append(json.loads(output_json))
 
         imprest_open = len(openImprest)
         imprest_app = len(AppImprest)
@@ -89,6 +154,22 @@ def dashboard(request):
         surrender_open = len(openSurrender)
         surrender_app = len(AppSurrender)
         surrender_rej = len(RejSurrender)
+
+        claim_open = len(openClaim)
+        claim_app = len(AppClaim)
+        claim_rej = len(RejClaim)
+
+        purchase_open = len(openPurchase)
+        purchase_app = len(AppPurchase)
+        purchase_rej = len(RejPurchase)
+
+        repair_open = len(openRepair)
+        repair_app = len(AppRepair)
+        repair_rej = len(RejRepair)
+
+        store_open = len(openStore)
+        store_app = len(AppStore)
+        store_rej = len(RejStore)
 
         leave_open = len(openLeave)
         leave_App = len(AppLeave)
@@ -130,7 +211,13 @@ def dashboard(request):
            "app_train": train_app, "rej_train": train_rej,
            "imprest_open": imprest_open, "imprest_app": imprest_app,
            "imprest_rej": imprest_rej, "surrender_open": surrender_open,
-           "surrender_app": surrender_app, "surrender_rej": surrender_rej
+           "surrender_app": surrender_app, "surrender_rej": surrender_rej,
+           "open_claim": claim_open, "app_claim": claim_app,
+           "rej_claim": claim_rej, "open_purchase": purchase_open,
+           "app_purchase": purchase_app, "rej_purchase": purchase_rej,
+           "open_repair": repair_open, "app_repair": repair_app,
+           "rej_repair": repair_rej, "open_store": store_open,
+           "app_store": store_app, "rej_store": store_rej
            }
     return render(request, 'main/dashboard.html', ctx)
 
